05011e4402f9ec679da5e47ae3dbb972414d14d2
Patryk-Kosmider <115877789+Patryk-Kosmider@users.noreply.github.com>
2023-01-11 19:18:17 +0100

    Update KalkulatorMacierzowy.cpp

--- KalkulatorMacierzowy.cpp
+++ KalkulatorMacierzowy.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 #include <iomanip>
 
@@ -9,9 +9,9 @@ void firstmenu()
 	//menu startowe
 
 	cout << setw(10) << "||--------------------------------------------------------------------||" << setw(10) << endl;
-	cout << setw(10) << "                      KALKULATOR MACIERZOWY 3x3                        " << setw(10) << endl;
+	cout << setw(10) << "                      KALKULATOR MACIERZOWY 3x3                         " << setw(10) << endl;
 	cout << endl;
-	cout << setw(10) << "    Podaj najpierw macierz A oraz macierz B, by przejsc do dzialan.  " << setw(10) << endl;
+	cout << setw(10) << "    Podaj najpierw macierz A oraz macierz B, by przejsc do dzialan.     " << setw(10) << endl;
 	cout << endl;
 	cout << setw(10) << "||--------------------------------------------------------------------||" << setw(10) << endl;
 	cout << endl;
@@ -22,19 +22,19 @@ void mainmenu()
 	//menu glowne
 
 
-	cout << "||--------------------------------------------------------------------||" << endl;
-	cout << "                      KALKULATOR MACIERZOWY    3x3                         " << endl;
+	cout << setw(10) << "||--------------------------------------------------------------------||   " << setw(10) << endl;
+	cout << setw(10) << "                      KALKULATOR MACIERZOWY    3x3                         " << setw(10) << endl;
 	cout << endl;
-	cout << "                        Wybierz dzialanie.                              " << endl;
+	cout << setw(10) << "                        Wybierz dzialanie.                                 " << setw(10) << endl;
 	cout << endl;
-	cout << "                       1. Dodawanie.                                      " << endl;
+	cout << setw(10) << "                       1. Dodawanie.                                       " << setw(10) << endl;
 	cout << endl;
-	cout << "                       2. Odejmowanie.                                      " << endl;
+	cout << setw(10) << "                       2. Odejmowanie.                                     " << setw(10) << endl;
 	cout << endl;
-	cout << "                       3. Mnozenie.                                      " << endl;
+	cout << setw(10) << "                       3. Mnozenie.                                        " << setw(10) << endl;
 	cout << endl;
-	cout << "                       4. Transpozycja                                     " << endl;
-	cout << "||--------------------------------------------------------------------||" << endl;
+	cout << setw(10) << "                       4. Transpozycja                                     " << setw(10) << endl;
+	cout << setw(10) << "||--------------------------------------------------------------------||   " << setw(10) << endl;
 	cout << endl;
 }
 
@@ -61,7 +61,7 @@ void seeMatrix(int m[3][3])
 
 // dodawanie 2 macierzy
 
-int sumMatrix(int m1[3][3], int m2[3][3])
+void sumMatrix(int m1[3][3], int m2[3][3])
 {
 	int mres[3][3]{};
 	for (int x = 0; x < 3; x++)
@@ -73,14 +73,13 @@ int sumMatrix(int m1[3][3], int m2[3][3])
 	}
 
 	seeMatrix(mres);
-
-	return mres[3][3];
+	
 }
 
 
 // dzielenie 2 macierzy
 
-int subMatrix(int m1[3][3], int m2[3][3])
+void subMatrix(int m1[3][3], int m2[3][3])
 {
 	int mres[3][3]{};
 	for (int x = 0; x < 3; x++)
@@ -93,12 +92,12 @@ int subMatrix(int m1[3][3], int m2[3][3])
 
 	seeMatrix(mres);
 
-	return mres[3][3];
+	
 }
 
 // transponowanie wybranej macierzy
 
-int transMatrix(int m1[3][3])
+void transMatrix(int m1[3][3])
 {
 	int mres[3][3]{};
 	for (int x = 0; x < 3; x++)
@@ -110,12 +109,12 @@ int transMatrix(int m1[3][3])
 	}
 
 	seeMatrix(mres);
-	return mres[3][3];
+	
 }
 
 // mnozenie macierzy
 
-int multiMatrix(int m1[3][3], int m2[3][3])
+void multiMatrix(int m1[3][3], int m2[3][3])
 {
 	int mres[3][3]{};
 	int res = 0;
@@ -131,7 +130,32 @@ int multiMatrix(int m1[3][3], int m2[3][3])
 		}
 	}
 	seeMatrix(mres);
-	return mres[3][3];
+	
+}
+
+// sprawdza poprawnosc wyboru opcji uzytkownika, w przypadku blednej podajemy ja jeszcze raz
+
+void validCheck(int& choice)
+{
+	cin >> choice;
+	while (!cin || choice < 1 || choice > 4)
+	{
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "Podaj poprawna opcje: " << endl;
+		cin >> choice;
+	}
+}
+
+void matrixElCheck(int& el)
+{
+	while(!cin)
+	{
+		cin.clear();
+		cin.ignore(INT_MAX, '\n');
+		cout << "Podaj poprawna liczbe: " << endl;
+		cin >> el;
+	}
 }
 
 
@@ -142,14 +166,20 @@ int main()
 	int matrix1[3][3]{};
 	int matrix2[3][3]{};
 
+	int el;
+
 	cout << "MACIERZ A" << endl;
 	cout << endl << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << "Podaj a" << i + 1 << j + 1 << ":" << endl;
-			cin >> matrix1[i][j];
+			cout << "Podaj A" << "[" << i+1 <<"]" << "[" << j + 1 << "]" << ":" << endl;
+			cin >> el;
+			matrixElCheck(el);
+			matrix1[i][j] = el;
+
+
 		}
 	} 
 	system("cls");
@@ -162,26 +192,27 @@ int main()
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			cout << "Podaj b" << i + 1 << j + 1 << ":" << endl;
+			cout << "Podaj B" << "[" << i + 1 << "]" << "[" << j + 1 << "]" << ":" << endl;
 			cin >> matrix2[i][j];
 		}
 	}
 	system("cls");
 
-	cout << "Podana macierz A: " << endl;
+	cout << "Podana macierz A: " << endl << endl;
 
 	seeMatrix(matrix1);
 
 	cout << endl;
 
-	cout << "Podana macierz B: " << endl;
+	cout << "Podana macierz B: " << endl << endl;
 
 	seeMatrix(matrix2);
 
 	mainmenu();
-	int choice;
-	cin >> choice;
 
+	cout << "Wybierz opcje [ 1 - 4 ]: ";
+	int choice;
+	validCheck(choice);
 	while (true)
 	{
 		if (choice == 1)
@@ -195,7 +226,9 @@ int main()
 			cout << endl << endl;
 			cout << "Macierz wynikowa: " << endl << endl;
 			sumMatrix(matrix1, matrix2);
-			break;
+			mainmenu();
+			cout << "Wybierz opcje [ 1 - 4 ]: ";
+			
 		}
 		if (choice == 2)
 		{
@@ -208,7 +241,9 @@ int main()
 			cout << endl << endl;
 			cout << "Macierz wynikowa: " << endl << endl;
 			subMatrix(matrix1, matrix2);
-			break;
+			mainmenu();
+			cout << "Wybierz opcje [ 1 - 4 ]: ";
+			
 		}
 		if (choice == 3)
 		{
@@ -221,43 +256,51 @@ int main()
 			cout << endl << endl;
 			cout << "Macierz wynikowa: " << endl << endl;
 			multiMatrix(matrix1, matrix2);
-			break;
+			mainmenu();
+			cout << "Wybierz opcje [ 1 - 4 ]: ";
 		}
 
 		if (choice == 4)
 		{
 			system("cls");
-			string ch;
+			int ch;
 			cout << "Macierz A: " << endl;
 			seeMatrix(matrix1);
 			cout << "Macierz B: " << endl;
 			seeMatrix(matrix2);
-			cout << "Wybierz jaka macierz, chcesz transponowac(A lub B): " << endl;
+			cout << "Wybierz jaka macierz, chcesz transponowac(1 dla A, 2 dla B): " << endl;
 			cin >> ch;
-			if (ch == "A" || ch == "a")
+			matrixElCheck(ch);
+			if (ch == 1)
 			{
+				system("cls");
 				cout << "Macierz przed dzialaniem: " << endl << endl;
 				seeMatrix(matrix1);
 				cout << "Macierz stransponowana: " << endl << endl;
 				transMatrix(matrix1);
 			}
-			if (ch == "B" || ch == "b")
+			else if (ch == 2)
 			{
-				cout << "Macierz przed dzialaniem: " << endl;
+				system("cls");
+				cout << "Macierz przed dzialaniem: " << endl << endl;
 				seeMatrix(matrix2);
-				cout << "Macierz stransponowana: " << endl;
+				cout << endl;
+				cout << "Macierz stransponowana: " << endl << endl;
 				transMatrix(matrix2);
 			} else
 			{
-				cout << "Zly wybor" << endl;
-				
+				cout << "Zla opcja, sprobuj ponownie:" << endl;
+
 			}
-
-			break;
+			mainmenu();
+			cout << "Wybierz opcje [ 1 - 4 ]: ";
+			validCheck(choice);
 		}
-		cout << "Podana opcja nie istnieje, sprobuj jeszcze raz." << endl;
-		cin >> choice;
+		else {
+			
+			validCheck(choice);
+		}
 	}
-
+	
 	return 0;
 }
